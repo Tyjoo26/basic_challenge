0c5bb67a36422aad4e151f8296f37e21686189d1
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-13 13:19:04 -0700

    Build form helper, added new create methods in controller, added resources to route for route helper

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,33 @@
 class StudentsController < ApplicationController
 
+
+  def index
+    @students = Student.all
+  end
+
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You've created a new Student"
+
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
   end
 end
